8d9b9cfa6d910d0d75217330ed7607babe83ebe5
Utkarsh Barnawal <utkarshbarnawal8@gmail.com>
2026-04-11 12:56:09 +0530

    changes done

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,10 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
-connectToDB()
-
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+connectToDB().then(() => {
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000")
+    })
+}).catch(err => {
+    console.error("Failed to connect to the database. Server not started.", err)
+});
