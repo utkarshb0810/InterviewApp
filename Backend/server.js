17a61403649930e01585cd4c49d07321b7191aad
Utkarsh Barnawal <utkarshbarnawal8@gmail.com>
2026-04-27 21:29:34 +0530

    fixed for deploy

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,9 @@ const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
 connectToDB().then(() => {
-    app.listen(3000, () => {
-        console.log("Server is running on port 3000")
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`)
     })
 }).catch(err => {
     console.error("Failed to connect to the database. Server not started.", err)
